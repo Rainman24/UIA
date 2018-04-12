b0c2016812fba673d1c9bc1580e1560b21f065a2
Alexandra Briceag <Computer1@Alexandras-MacBook-Pro-2.local>
2018-04-13 00:28:46 +0200

    Refactoring Searchbar Route

--- app.js
+++ app.js
@@ -17,112 +17,53 @@ app.get('/', (req, res)=>{
 
 app.post('/searchbar', function(req, res){
 
+	var searchres = req.body.searchreq
+
+	var searchresLo = searchres.toLowerCase()
+
+	var allmatches = new Object()
+
+	var because = 'Sorry no matching users were found'
+	var foundmatches
 
 	fs.readFile('./public/json/' + 'users.json', 'utf8', function(err,data){
 		if(err) {throw err}
-		
-		// const fullName = /^[a-z]{1,20}(-|')?[a-z]{0,20}?\s?[a-z]{0,20}?(-|')?[a-z]{0,20}?$/ig;
-
-		// const fullName2 = /^[a-z]{1,20}(-?[a-z]{0,20}|'?[a-z]{0,20})(\s[a-z]{1,20}|\s[0-9]{1,20})?(-?[a-z]{0,20}|'?[a-z]{0,20})$/gi;
 
 		var userNames = JSON.parse(data)
 
-		var searchres = req.body.searchreq
-		console.log(searchres)
-		var searchresLo = searchres.toLowerCase()
-		var searchsplit = searchres.toLowerCase().split(' '),
-			searchsplitDash = searchres.toLowerCase().split('-')
+		for(i=0;i<userNames.length;i++){
 
-		var allmatches = new Object()
+			var ufirstname = userNames[i].firstname.toLowerCase()
+			var ulastname = userNames[i].lastname.toLowerCase()
+			var fullUserName = `${ufirstname} ${ulastname}`
 
-		var because = 'Sorry no matching users were found'
-		var foundmatches
+			if(searchresLo==fullUserName){
 
+				allmatches['firstname']=ufirstname
+				allmatches['lastname']=ulastname
 
-			for(i=0;i<userNames.length;i++){
+				foundmatches = `firstname=${allmatches.firstname}&lastname=${allmatches.lastname}`
 
-				var ufirstname = userNames[i].firstname.toLowerCase()
-				var ulastname = userNames[i].lastname.toLowerCase()
-				var fullUserName = `${ufirstname} ${ulastname}`
+			} else if(searchresLo!==fullUserName){
 
-				var fnameWithSpaces = ufirstname.split(' '),
-					fnameWithDashes = ufirstname.split('-')
-				var lnameWithSpaces = ulastname.split(' '),
-					lnameWithDashes = ulastname.split('-')
-
-				if(searchresLo==fullUserName){
-
-					console.log(fullUserName)
-
-					allmatches['firstname']=ufirstname
-					allmatches['lastname']=ulastname
-
-					foundmatches = `firstname=${allmatches.firstname}&lastname=${allmatches.lastname}`
-
-				} else {
-
-					if(searchsplit[0]===ufirstname&&searchsplit[1]===ulastname){
-
-						allmatches['firstname']=searchsplit[0]
-						allmatches['lastname']=searchsplit[1]
-
-						foundmatches = `firstname=${allmatches.firstname}&lastname=${allmatches.lastname}`
-					}
-
-					else if(searchsplit.length===1||searchsplitDash.length===1){
-
-						if(fnameWithSpaces.length>1||lnameWithSpaces.length>1){
-							
-							fnameWithSpaces.forEach(function(fname){
-								
-								if(searchsplit[0]===fname){
-									allmatches['firstname']=ufirstname
-									foundmatches = `firstname=${allmatches.firstname}`
-								}
-							})
-							
-							lnameWithSpaces.forEach(function(lname){
-								
-								if(searchsplit[0]===lname){
-									allmatches['lastname']=ulastname
-									foundmatches = `lastname=${allmatches.lastname}`
-								}
-							})
-						}
-
-						if(fnameWithDashes.length>1||lnameWithDashes.length>1){
-							
-							fnameWithDashes.forEach(function(fname){
-								
-								if(searchsplit[0]===fname){
-									allmatches['firstname']=ufirstname
-									foundmatches = `firstname=${allmatches.firstname}`
-								}
-							})
-							
-							lnameWithDashes.forEach(function(lname){
-								
-								if(searchsplit[0]===lname){
-									allmatches['lastname']=ulastname
-									foundmatches = `lastname=${allmatches.lastname}`
-								}
-							})
-						}
-
-						if(searchsplit[0]===ufirstname){
-										
-							allmatches['firstname']=searchsplit[0]
-							foundmatches = `firstname=${allmatches.firstname}`
-
-						} else if(searchsplit[0]===ulastname){
-
-							allmatches['lastname']=searchsplit[0]
-							
-							Object.getOwnPropertyNames(allmatches).length<2 ? foundmatches = `lastname=${searchsplit[0]}` : foundmatches = `firstname=${allmatches.firstname}&lastname=${allmatches.lastname}`
-						}
-					}
+				if(searchresLo==ufirstname){
+					allmatches['firstname']=searchresLo
+				}
+				if(searchresLo==ulastname){
+					allmatches['lastname']=searchresLo
 				}
 			}
+
+		}
+
+		if(Object.getOwnPropertyNames(allmatches).length==1){
+				
+			allmatches.hasOwnProperty('firstname') ? foundmatches = `firstname=${allmatches.firstname}` : foundmatches = `lastname=${allmatches.lastname}`
+				
+		} else if(Object.getOwnPropertyNames(allmatches).length==2){
+
+			foundmatches = `firstname=${allmatches.firstname}&lastname=${allmatches.lastname}`
+		}
 
 		Object.keys(allmatches).length !== 0 ? res.redirect(`/userlist/user?${foundmatches}`) : res.redirect(`/adduser?message=${because}`)
 	
